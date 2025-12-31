59095a33f046f6823e2033928f7b6781e399c031
SnehaBasugade <snehabasugade1111@gmail.com>
2025-12-31 00:25:42 -0800

    DAY=42 Sets

---
+++ 10 sets.py
@@ -0,0 +1,13 @@
+set1={10,30,59,29,390,25}
+print(set1)
+#for n in set1:
+ #   print(n)
+print(set1)
+#set1.add(100)
+#set1.remove(29)
+#set1.pop()
+#set1.discard(25)
+#set1.clear()
+list1=[10,25,78,87]
+set1.update(list1)
+print(set1)
